4f123cc0555cc840c87c602ca96a289b86345eb6
Roger Bassons <rogerbassons@gmail.com>
2015-09-03 11:28:53 +0200

    Working Basic Menu
    When the app is launched it shows a simple button. By pressing the button
    the game starts

--- app/src/main/java/com/rogerbassonsrenart/paddletennis/MenuActivity.java
+++ app/src/main/java/com/rogerbassonsrenart/paddletennis/MenuActivity.java
@@ -1,12 +1,11 @@
 package com.rogerbassonsrenart.paddletennis;
 
-import android.support.v7.app.ActionBarActivity;
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.View;
 
-
-public class MenuActivity extends ActionBarActivity {
+public class MenuActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,25 +13,8 @@ public class MenuActivity extends ActionBarActivity {
         setContentView(R.layout.activity_menu);
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_menu, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+    public void startGame(View view) {
+        Intent intent = new Intent(this, GameActivity.class);
+        startActivity(intent);
     }
 }
